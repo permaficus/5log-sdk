b6dd1d61c3036b8a36fe1f613b47d8d0d104803c
permaficus <abukhalif2019@gmail.com>
2024-06-02 00:52:49 +0700

    chore(libs): add stack trace parser and verbose output

--- libs/logging.ts
+++ libs/logging.ts
@@ -5,7 +5,13 @@
  */
 
 import HttpClient from "./httpClient"
-import type { ErrorPayload, FilogInitObject, FilogTransportConfig } from "./types"
+import type { 
+    ErrorPayload, 
+    FilogInitObject, 
+    FilogTransportConfig
+} from "./types"
+import * as stp from "stacktrace-parser"
+import chalk from "chalk"
 
 class filog {
     private args: FilogTransportConfig
@@ -33,13 +39,30 @@ class filog {
      *   errorDescription: 'error-message'
      * }
      */
-    write (error: ErrorPayload): void {
+    write (error: ErrorPayload, verbose?: boolean): void {
+
         let transport: FilogInitObject[] = this.args.filter((field: FilogInitObject) => field.logType === error.logLevel)
         if (transport.length === 0) {
             transport = this.args.filter((field: FilogInitObject) => field.logType === 'ANY')
         }
+
+        // if error.errorDescrption instanceof Error then start parsing the error stack
+        let rawObjectStack: Array<stp.StackFrame> = [];
+        let _errorDescription: string = '';
+        if (error.errorDescription instanceof Error) {
+            // parsing the error stack
+            rawObjectStack = stp.parse(error.errorDescription.stack)
+            // combining error
+            _errorDescription = `${error.errorDescription.name}: ${error.errorDescription.message}\n`
+            _errorDescription = _errorDescription + `\xa0\xa0\xa0[${rawObjectStack[0].methodName}] ${rawObjectStack[0].file}\n`
+            _errorDescription = _errorDescription + `\xa0\xa0\xa0[${rawObjectStack[1].methodName}] ${rawObjectStack[1].file}:${rawObjectStack[1].lineNumber}:${rawObjectStack[1].column}`
+            // update errorDescription
+            error.errorDescription = _errorDescription
+        }
+
         const connector = new HttpClient(transport[0].client_id, transport[0].url);
         connector.send(error)
+        if (verbose) console.log(chalk.redBright(`\n${_errorDescription}\n`))
     }
 }
 
