1d9327cf9e46619463562e2f1573ac9abcb93515
permaficus <abukhalif2019@gmail.com>
2024-06-02 10:16:42 +0700

    chore: refactor some codes in library

--- libs/logging.ts
+++ libs/logging.ts
@@ -7,6 +7,7 @@
 import HttpClient from "./httpClient"
 import type { 
     ErrorPayload, 
+    ExtraWriteArguments, 
     FilogInitObject, 
     FilogTransportConfig
 } from "./types"
@@ -25,6 +26,7 @@ class filog {
     /**
      * Send this error payload to API service
      * @param {ErrorPayload} error - See example
+     * @param {Object} {object} - Properties: default = { verbose: false, originalError: error }
      * @example
      * {
      *   logLevel: 'ERROR',
@@ -36,33 +38,34 @@ class filog {
      *      app_version: 'package-version',
      *      package_name: 'project/package_name'
      *   },
-     *   errorDescription: 'error-message'
+     *   errorDescription: error
      * }
      */
-    write (error: ErrorPayload, verbose?: boolean): void {
+    write (error: ErrorPayload, { verbose, originalError }: ExtraWriteArguments): void {
 
         let transport: FilogInitObject[] = this.args.filter((field: FilogInitObject) => field.logType === error.logLevel)
         if (transport.length === 0) {
-            transport = this.args.filter((field: FilogInitObject) => field.logType === 'ANY')
+            transport = this.args.filter((field: FilogInitObject) => field.logType.toUpperCase() === 'ANY')
         }
 
-        // if error.errorDescrption instanceof Error then start parsing the error stack
+        // if errorDescription or originalError instanceof Error then start parsing the error stack
         let rawObjectStack: Array<stp.StackFrame> = [];
         let _errorDescription: string = '';
-        if (error.errorDescription instanceof Error) {
+        if (error.errorDescription instanceof Error || originalError instanceof Error) {
             // parsing the error stack
-            rawObjectStack = stp.parse(error.errorDescription.stack)
+            rawObjectStack = error.errorDescription ? stp.parse(error.errorDescription.stack) : stp.parse(originalError.stack)
             // combining error
-            _errorDescription = `${error.errorDescription.name}: ${error.errorDescription.message}\n`
+            _errorDescription = `${error.errorDescription ? error.errorDescription.name : originalError.name}: ${error.errorDescription 
+                ? error.errorDescription.message : originalError.message}\n`
             _errorDescription = _errorDescription + `\xa0\xa0\xa0[${rawObjectStack[0].methodName}] ${rawObjectStack[0].file}\n`
             _errorDescription = _errorDescription + `\xa0\xa0\xa0[${rawObjectStack[1].methodName}] ${rawObjectStack[1].file}:${rawObjectStack[1].lineNumber}:${rawObjectStack[1].column}`
             // update errorDescription
             error.errorDescription = _errorDescription
         }
 
+        if (verbose === 'true') console.log(chalk.redBright(`\n${_errorDescription}\n`))
         const connector = new HttpClient(transport[0].client_id, transport[0].url);
         connector.send(error)
-        if (verbose) console.log(chalk.redBright(`\n${_errorDescription}\n`))
     }
 }
 
