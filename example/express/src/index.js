ae54422ef1fb59c2b8ec40287ced2e453c631492
permaficus <abukhalif2019@gmail.com>
2024-06-03 11:56:58 +0700

    chore: delete some folder

--- example/express/src/index.js
+++
@@ -1,8 +0,0 @@
-import { httpServer, serviceInit } from "./libs/service.init.js";
-
-(() => {
-    serviceInit;
-    httpServer.listen(4020, () => {
-        console.log(`\nHTTP Server run at: 4020\n`)
-    })
-})()
